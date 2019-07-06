04e639ac3e2f5a01d04611941d80b633bec295cb
Roberto Mejia <135RobertoMejia@gmail.com>
2019-07-06 08:03:22 -0700

    2nd save, it works but still need to add the modal part to add data to json object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
 
+interface Email {
+  name: string;
+  date: number;
+  subject: string;
+  comment: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,6 +18,37 @@ export class AppComponent {
   x = 0;
   y = 0;
 
+  public email : Email[];
+  public newEmail: Email;
+
+  constructor() {
+    this.email = [{
+      name: 'Robert',
+      date: 1993, 
+      subject:'First Email', 
+      comment: 'This is an email. This is a cool email.'
+    }];
+
+    this.newEmail = {
+      name: 'Jorge', 
+      date: 1991, 
+      subject: 'Hello', 
+      comment: "Hello how are you???"
+    };
+
+    this.email.push(this.newEmail);
+
+    this.newEmail = {
+      name: 'Chung',
+      date: 2018,
+      subject: 'What is Chungus?',
+      comment: 'According to Urban Dictionary, one of the top definitions for Chungus is An overweight giant earth destroying, god killing rabbit. In the memes, its basically a picture of a fat Bugs Bunny. ... The word "chungus" was coined by video game journalist Jim Sterling years before the meme went viral.'
+    };
+
+    this.email.push(this.newEmail);
+
+
+  }
 
   goo(){
 
@@ -30,6 +68,16 @@ export class AppComponent {
   }
 
 
-
 }
+
+  ///This method adds poeples information to the json object
+  addEmail(namee: string, datee: number, subjectt: string, commentt:string) {
+    if (namee) {
+      this.newEmail = { name: namee, date: datee, subject: subjectt, comment: commentt };
+      this.email.push(this.newEmail);
+    }
+
+  }
+
+
 }
